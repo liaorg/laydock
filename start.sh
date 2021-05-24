a21fc83a67bbc7d9248b941801f4dd793a646619
廖荣贵 <liaorg@heidun.net>
2021-05-24 10:57:49 +0800

    修正启动命令

--- start.sh
+++ start.sh
@@ -2,30 +2,32 @@
 
 # set -x
 
-echo "*** 启动 ssh ***"
-flag = `ps aux|grep "sshd"|grep -v "grep"|wc -l`
-if [ $flag == 0 ]
+echo "--- 启动 ssh ---"
+flag=`ps aux|grep "sshd"|grep -v "grep"|wc -l`
+if [ "$flag" = "0" ]; then
     service ssh start
 fi
 service ssh status
 
 echo "================================"
 
-echo "*** 启动 docker ***"
-flag = `ps aux|grep "dockerd"|grep -v "grep"|wc -l`
-if [ $flag == 0 ]
+echo "--- 启动 docker ---"
+flag=`ps aux|grep "dockerd"|grep -v "grep"|wc -l`
+if [ "$flag" = "0" ]; then
     service docker start
+else
+    echo " * docker is running"
 fi
 
 echo "================================"
 
-echo "*** 启动应用 ***"
-if [ "$1" == "phpcli" ]; then
+echo "--- 启动应用 ---"
+if [ "$1" = "phpcli" ]; then
     docker-compose -f docker-compose.cli.yml up -d
 else
     docker-compose up -d
 fi
-    
+
 echo "================================"
 
 # 查看启动的容器
